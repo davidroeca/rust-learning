095237c87aadb000e94941a8234a529b6b380f49
davidroeca <david.roeca@gmail.com>
2015-10-21 15:02:47 -0400

    add syntax parser--have some errors with hashmap, will fix later

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,5 +1,5 @@
-use std::io::prelude::*;
 use std::io;
+use std::io::prelude::*;
 use std::str::FromStr;
 use std::collections::HashMap;
 
@@ -13,11 +13,24 @@ enum Token {
     Subtract,
     Multiply,
     Divide,
-    Number { value: f64 },
-    Variable { name: String },
+    Number(f64),
+    Variable(String),
     Assign,
     Invalid
 }
+
+enum BinaryOp {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+enum Expression {
+    Single(f64),
+    Full(Box<Expression>, BinaryOp, Box<Expression>)
+}
+
 
 fn prompt(input_str: &str) {
     let input_string = String::from(input_str);
@@ -80,10 +93,10 @@ fn define_token(token: &str) -> Token {
     let re_var = Regex::new(r"^([a-z]|[A-z])(\d|[a-z]|[A-Z])*$").unwrap();
     let re_num = Regex::new(r"^\d*\.??\d+$").unwrap();
     if re_var.is_match(token) {
-        return Token::Variable { name: token.to_string() } ;
+        return Token::Variable(token.to_string());
     }
     else if re_num.is_match(token) {
-        return Token::Number { value: token.parse::<f64>().ok().expect("err")};
+        return Token::Number(token.parse::<f64>().ok().expect("err"));
     }
     else {
         return match token {
@@ -99,25 +112,67 @@ fn define_token(token: &str) -> Token {
     }
 }
 
-fn parse_tokens(tokens: Vec<Token>, vars: &HashMap<String, f64>, depth: i32) {
-    for token in tokens {
-        match token {
-            Token::LParen => println!("Left paren"),
-            Token::RParen => println!("Right paren"),
-            Token::Add => println!("Add"),
-            Token::Subtract => println!("Subtract"),
-            Token::Multiply => println!("Multiply"),
-            Token::Divide => println!("Divide"),
-            Token::Number { value: v } => println!("Number: {}", v),
-            Token::Variable { name: n } => println!("Variable: {}", n),
-            Token::Assign => println!("Assign"),
-            Token::Invalid => println!("Invalid")
+fn parse_tokens(tokens: Vec<Token>, vars: &mut HashMap<String, f64>) -> Expression {
+    let length = tokens.len();
+    let max_index = length - 1;
+    fn parse_expression(index: usize, max_index: usize, tokens: Vec<Token>,
+                        vars: &mut HashMap<String, f64>) -> (Expression, usize) {
+        if index > max_index {
+            panic!("Index out of range");
         }
+        let (LHS, op_index) = match tokens[index] {
+            Token::LParen => parse_expression(index + 1, max_index, tokens, vars),
+            Token::Number(n) => (Expression::Single(n), index + 1),
+            Token::Variable(v) => (Expression::Single(vars[v]), index + 1),
+            _ => panic!("Invalid Token")
+        };
+        if op_index > max_index {
+            panic!("Index out of range");
+        }
+        let (OP, rhs_index) = (match tokens[op_index] {
+            Token::Add => BinaryOp::Add,
+            Token::Subtract => BinaryOp::Subtract,
+            Token::Divide => BinaryOp::Divide,
+            Token::Multiply => BinaryOp::Multiply
+        }, op_index + 1);
+        if rhs_index > max_index {
+            panic!("Index out of range");
+        }
+        let (RHS, next_index) = match tokens[rhs_index] {
+            Token::LParen => parse_expression(rhs_index + 1, max_index, tokens, vars),
+            Token::Number(n) => (Expression::Single(n), op_index + 1),
+            Token::Variable(v) => (Expression::Single(vars.get(v)), op_index + 1),
+            _ => panic!("Invalid Token")
+        };
+        if next_index > max_index {
+            panic!("Index out of range");
+        }
+        match tokens[next_index] {
+            Token::RParen => (),
+            _ => panic!("Right parenthesis expected")
+        };
+        return (Expression::Full(Box::new(LHS), OP, Box::new(RHS)), next_index + 1);
     }
+    let (expression, _) = parse_expression(0, max_index, tokens, vars);
+    expression
+    //for i in 0..length {
+        //match tokens[i] {
+            //Token::LParen => println!("Left paren"),
+            //Token::RParen => println!("Right paren"),
+            //Token::Add => println!("Add"),
+            //Token::Subtract => println!("Subtract"),
+            //Token::Multiply => println!("Multiply"),
+            //Token::Divide => println!("Divide"),
+            //Token::Number(v) => println!("Number: {}", v),
+            //Token::Variable(ref n) => println!("Variable: {}", n),
+            //Token::Assign => println!("Assign"),
+            //Token::Invalid => println!("Invalid")
+        //}
+    //}
 }
 
 fn main() {
-    let vars: HashMap<String, f64> = HashMap::new();
+    let mut vars: HashMap<String, f64> = HashMap::new();
     loop {
         let input = get_user_input("> ");
         if input == "help" {
@@ -128,6 +183,6 @@ fn main() {
         }
         let undefined_tokens = tokenize_input(input.as_ref());
         let defined_tokens = define_tokens(undefined_tokens);
-        parse_tokens(defined_tokens, &vars, 1);
+        parse_tokens(defined_tokens, &mut vars);
     }
 }
