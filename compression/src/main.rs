940848abe325b5023966174f889535a53c5700ad
davidroeca <david.roeca@gmail.com>
2015-11-18 19:52:29 -0500

    further prototyping

--- compression/src/main.rs
+++ compression/src/main.rs
@@ -1,10 +1,23 @@
 use std::io::{BufReader, BufWriter};
 use std::collections::HashMap;
 
-fn encode(input: &String, output: &mut String) {
-    
+fn encode<T>(input: &mut T) -> Vec<u8> {
+    //
+    // T must implement the Read trait
+    //
+    let mut codes = HashMap::new();
+    let mut output = Vec::new();
+    loop {
+        let mut buf = [0; 1]
+        let bytes_read = input.read(&mut buf).ok().expect("Bad read.");
+        output.push(codes.entry(buf));
+        if !bytes_read {
+            break;
+        }
+    }
+    output
 }
 
 fn main() {
 
-}
+
