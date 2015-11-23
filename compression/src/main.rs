b15da7346f45df25a9f207aa8ad41aa1f984ac09
David <david.roeca@gmail.com>
2015-11-23 02:42:02 -0500

    added further modifications; write decisions are still in the works

--- compression/src/main.rs
+++ compression/src/main.rs
@@ -1,6 +1,9 @@
 use std::io::{Read, Write};
 use std::collections::HashMap;
 use std::char;
+use std::str::FromStr;
+use std::u32;
+use std::fmt;
 //
 // This code references: http://marknelson.us/2011/11/08/lzw-revisited/
 // It attempts to implement this algorithm in a rusty manner.
@@ -9,12 +12,9 @@ use std::char;
 // ------------------------------------------------------------------
 // LZW driver functions
 // ------------------------------------------------------------------
-fn lzw_compress<T: Read, U: Write>(input: &T, output: &mut U) {
-    //
-    // Type T must implement the Read trait
-    // Type U must implement the Write trait
-    //
-
+fn lzw_compress<T: Read, U: Write>(input: &T,
+                                   output: &mut U,
+                                   max_code: u32) {
     // Initialize the code dictionary
     let mut codes: HashMap<String, u32> = HashMap::new();
     for val in 0..256 {
@@ -27,35 +27,71 @@ fn lzw_compress<T: Read, U: Write>(input: &T, output: &mut U) {
     let mut current_string = String::new();
 
     // Compress
-    loop {
-        // Read the next byte, break from loop if nothing more to read
-        let mut buf = [0; 1];
-        let bytes_read = input.read(&mut buf).ok().expect("Bad read.");
-        if !bytes_read {
-            break;
-        }
-        // Add the byte to current string
-        current_string.push(buf);
-        match codes.get(current_string) {
+    for c in input.chars() {
+        // figure out a better numeric write
+        current_string.push(c.unwrap());
+        match codes.get(&current_string) {
             None => {
-                codes.insert(current_string, next_code);
-                next_code += 1;
-
+                if (next_code <= max_code) {
+                    codes.insert(current_string, next_code);
+                    next_code += 1;
+                }
                 // Get code for string of known value; write
                 current_string.pop();
-                output.write("{}", codes.get(current_string).unwrap());
+                write!(output, "{:?}",
+                            codes.get(&current_string).expect("error"));
 
                 // Reset string to current character, continue search
                 current_string.clear();
-                current_string.push(buf);
+                current_string.push(c.unwrap());
             },
             Some(_)=> ()
         };        
     }
-    // Write final code
-    output.write("{}", codes.get(current_string).unwrap());
+    try!(output.write_fmt(format_args!(
+                "{}",
+                codes
+                .get(&current_string)
+                .unwrap())));
 }
 
-fn lzw_decompress<T: Read, U: Write>(input: &T, output: &mut U) {
+fn lzw_decompress<T: Read, U: Write>(input: &T,
+                                     output: &mut U,
+                                     max_code: u32) {
+    // Initialize the string dictionary
+    let mut strings: HashMap<u32, String> = HashMap::new();
+    for key in 0..256 {
+        let val = char::from_u32(key).expect("Character Error.").to_string();
+        strings.insert(key, val);
+    }
+
+    // define the mutable loop variables
+    let mut next_code = 257;
+    let mut previous_string = String::new();
+    // Decompress
+    for c in input.chars() {
+        // figure out a better numeric read
+        let current_code = u32::from_str(c.to_string().as_ref()).unwrap();
+        match strings.get(current_code) {
+            None => {
+                let prev_start = previous_string.chars()
+                    .nth(0).unwrap().to_string();
+                strings.insert(current_code, previous_string + &prev_start);
+            },
+            Some(_) => ()
+        };
+        let current_string = strings.get(current_code).unwrap();
+        output.write("{}", current_string);
+        if !previous_string.is_empty() && next_code <= max_code {
+            let current_start = current_string.chars()
+                .nth(0).unwrap().to_string();
+            strings.insert(next_code, previous_string + &current_start);
+            next_code += 1;
+        }
+        previous_string = current_string.clone();
+    }
 }
 
+fn main() {
+    println!("Hi");
+}
